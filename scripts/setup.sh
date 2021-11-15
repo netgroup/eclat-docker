1f38253ce3472e6c26b29e190aa5025ad3e5b768
Stefano <stefano.salsano@uniroma2.it>
2021-11-15 23:02:35 +0100

    setup scripts and submodules removal

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+
+git clone git@github.com:netgroup/eclat-docker.git
+cd eclat-docker
+git checkout stefano2
+git clone git@github.com:netgroup/eclat-daemon.git
+cd eclat-daemon
+git checkout stefano2
