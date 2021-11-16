63710b2c9ad818b1a5016e1b704ce0f2426486f2
Stefano <stefano.salsano@uniroma2.it>
2021-11-16 02:20:07 +0100

    fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
-git clone git@github.com:netgroup/eclat-docker.git
-cd eclat-docker
-git checkout stefano2
+#git clone git@github.com:netgroup/eclat-docker.git
+#cd eclat-docker
+#git checkout stefano2
 git clone git@github.com:netgroup/eclat-daemon.git
 cd eclat-daemon
 git checkout stefano2
