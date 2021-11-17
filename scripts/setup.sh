ad83ab6af0e377c567ffb593b1872c8dc818f1f2
Stefano Salsano <stefano.salsano@uniroma2.it>
2021-11-17 08:19:05 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,12 @@ git clone git@github.com:netgroup/eclat-daemon.git
 cd eclat-daemon
 git checkout stefano2
 
-#on windows/WSL the following scripts and commands cannot be w
+#on windows/WSL the following commands cannot be executed on
+#the windows host, they must be executed inside the wsl container
+#otherwise the symbolic links does not work
+#as a workaround, we use a script in eclat-daemon to delete and
+#clone again
+
 git clone git@github.com:netgroup/hike_v3.git
 cd hike_v3
 git checkout stefano2
