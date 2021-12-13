6c912ce31f36fc032778d62bad254650bdd03005
Giulio Sidoretti <giuliosidoretti@gmail.com>
2021-12-13 15:42:57 +0100

    change setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,5 +16,5 @@ git checkout stefano2
 
 git clone git@github.com:netgroup/hike_v3.git
 cd hike_v3
-git checkout stefano_post_v3_2
+git checkout stefano2_post_v3_2
 git submodule update --init --recursive
